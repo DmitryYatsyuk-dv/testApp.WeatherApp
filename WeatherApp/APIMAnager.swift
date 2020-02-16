03bdb2c837fb47195e06118b3728a366cfc71ecd
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-02-16 15:10:17 +0300

    Added: extension/ default implementation/ Error Manager

--- WeatherApp/APIMAnager.swift
+++ WeatherApp/APIMAnager.swift
@@ -23,7 +23,6 @@ protocol APIManager {
     var sessionConfiguration: URLSessionConfiguration { get }
     var session: URLSession { get }
     
-    func JSONTaskWith(request: URLRequest, completionHandler: JSONComplitionHandler) -> JSONTask
     
     func fetch<T>(request: URLRequest,
                   parse: ([String: AnyObject]?) -> T?,
@@ -33,3 +32,64 @@ protocol APIManager {
     
 }
 
+extension APIManager {
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping JSONComplitionHandler) -> JSONTask {
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            // If not HTTP
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                let error = NSError(domain: YATNetworkingErrorDomain,
+                                    code: MissingHTTPResponseError,
+                                    userInfo: userInfo)
+                
+                completionHandler(nil, nil, error)
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: AnyObject]
+                        completionHandler(json, HTTPResponse, nil)
+                    } catch let error as NSError {
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default:
+                    print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
+    func fetch<T>(request: URLRequest,
+                  parse: @escaping ([String: AnyObject]?) -> T?,
+                  completionHandler: @escaping (APIResult<T>) -> Void) {
+        
+        let dataTask = JSONTaskWith(request: request) { (json, response, error) in
+            
+            guard let json = json else {
+                if let error = error {
+                    completionHandler(.Failure(error))
+                }
+                return
+            }
+            if let value = parse(json) {
+                completionHandler(.Success(value))
+            } else {
+                let error = NSError(domain: YATNetworkingErrorDomain, code: 200, userInfo: nil)
+                completionHandler(.Failure(error))
+            }
+        }
+        dataTask.resume()
+    }
+}
---
+++ WeatherApp/ErrorManager.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorManager.swift
+//  WeatherApp
+//
+//  Created by Lucky on 16/02/2020.
+//  Copyright © 2020 DmitriyYatsyuk. All rights reserved.
+//
+
+import Foundation
+
+public let YATNetworkingErrorDomain = "ru.yatsyuk.dev@mail.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
+public let UnexpectedResponseError = 200
+
+
