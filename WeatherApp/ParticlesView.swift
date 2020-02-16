595a7faa84ed1bbae61ccb3a1dd08340958f0e5d
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-02-17 01:11:29 +0300

    Set parameters ParticlesView

--- WeatherApp/ParticlesView.swift
+++ WeatherApp/ParticlesView.swift
@@ -11,4 +11,24 @@ import SpriteKit
  
 class ParticlesView: SKView {
 
+    override func didMoveToSuperview() {
+        
+        let scene = SKScene(size: self.frame.size)
+        scene.backgroundColor = UIColor.clear
+        self.presentScene(scene)
+        
+        // Set particlesView.background is clear
+        self.allowsTransparency = true
+        self.backgroundColor = UIColor.clear
+        
+        // Place particles on our stage
+        if let particles = SKEmitterNode(fileNamed: "ParticleScene.sks") {
+            particles.position = CGPoint(x: self.frame.size.width / 2,
+                                         y: self.frame.size.height)
+            // Set limits
+            particles.particlePositionRange = CGVector(dx: self.bounds.size.width,
+                                                       dy: 0)
+            scene.addChild(particles)
+        }
+    }
 }
